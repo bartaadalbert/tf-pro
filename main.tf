f1f533ad8596acb96e47ee5c6895fe700f36b8a5
Adalbert Barta <adalbertbarta@gmail.com>
2023-06-20 14:39:22 +0000

    terraform gke and fluxcd

--- main.tf
+++ main.tf
@@ -1,19 +1,10 @@
-# module "gke_cluster" {
-#   source         = "github.com/bartaadalbert/tf-google-gke-cluster"
-#   GOOGLE_REGION  = var.GOOGLE_REGION
-#   GOOGLE_PROJECT = var.GOOGLE_PROJECT
-#   GKE_NUM_NODES  = 3
-# }
-
-module "kind_cluster" {
-
-  # source = "github.com/den-vasyliev/tf-kind-cluster"
-  source            = "github.com/bartaadalbert/tf-kind-cluster"
-  KIND_CLUSTER_NAME = var.KIND_CLUSTER_NAME
-  NUM_MASTERS       = var.NUM_MASTERS
-  NUM_WORKERS       = var.NUM_WORKERS
-
+module "gke_cluster" {
+  source         = "github.com/bartaadalbert/tf-google-gke-cluster?ref=kubeconfig"
+  GOOGLE_REGION  = var.GOOGLE_REGION
+  GOOGLE_PROJECT = var.GOOGLE_PROJECT
+  GKE_NUM_NODES  = 3
 }
+
 
 terraform {
   backend "gcs" {
@@ -23,7 +14,7 @@ terraform {
 }
 
 module "github_repository" {
-  source                   = "github.com/den-vasyliev/tf-github-repository"
+  source                   = "github.com/bartaadalbert/tf-github-repository?ref=develop"
   github_owner             = var.GITHUB_OWNER
   github_token             = var.GITHUB_TOKEN
   repository_name          = var.FLUX_GITHUB_REPO
@@ -32,12 +23,11 @@ module "github_repository" {
 }
 
 module "flux_bootstrap" {
-  # source            = "github.com/den-vasyliev/tf-fluxcd-flux-bootstrap"
   source            = "github.com/bartaadalbert/tf-fluxcd-flux-bootstrap"
   github_repository = "${var.GITHUB_OWNER}/${var.FLUX_GITHUB_REPO}"
-  github_token      = "${var.GITHUB_TOKEN}"
+  github_token      = var.GITHUB_TOKEN
   private_key       = module.tls_private_key.private_key_pem
-  config_path       = module.kind_cluster.kubeconfig
+  config_path       = module.gke_cluster.kubeconfig_path
 }
 
 module "tls_private_key" {
